bf64dd5471cf1d8f0e37a569b4e9595353338c59
Vinh241 <nguyenmanhvinh123@gmail.com>
2025-04-05 15:46:26 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
-
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [HomeComponent],
 })
-export class AppModule { }
+export class AppModule {}
